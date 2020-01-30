2a0ddcc66862ecbc43db50c69fa5c4b4dc639a91
Seth NeJame <seth@worldfavor.com>
2020-01-30 12:57:32 +0100

    Get requests to api/camps now return mapped camp collections

--- starting/TheCodeCamp/Controllers/CampsController.cs
+++ starting/TheCodeCamp/Controllers/CampsController.cs
@@ -1,19 +1,23 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
 using TheCodeCamp.Data;
+using TheCodeCamp.Models;
 
 namespace TheCodeCamp.Controllers
 {
     public class CampsController : ApiController
     {
         private readonly ICampRepository _repository; // field that holds repository data
-        public CampsController(ICampRepository repository) // constructor instantiates a field from ICampRepo
+        private readonly IMapper _mapper;
+        public CampsController(ICampRepository repository, IMapper mapper) // constructor instantiates a field from ICampRepo
         {
             _repository = repository;
+            _mapper = mapper;
         }
         public async Task<IHttpActionResult> Get()
         {
@@ -21,7 +25,9 @@ namespace TheCodeCamp.Controllers
             {
                 var result = await _repository.GetAllCampsAsync();
 
-                return Ok(result);
+                // Mapping
+                var mappedResult = _mapper.Map<IEnumerable<CampModel>>(result);
+                return Ok(mappedResult);
             }
             catch (Exception ex)
             {
--- starting/TheCodeCamp/Data/CampMappingProfile.cs
+++ starting/TheCodeCamp/Data/CampMappingProfile.cs
@@ -1,13 +1,18 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TheCodeCamp.Models;
 
 namespace TheCodeCamp.Data
 {
-    public class CampMappingProfile
+    public class CampMappingProfile : Profile
     {
-        
+        public CampMappingProfile()
+        {
+            CreateMap<Camp, CampModel>();
+        }
     }
 }

9a84892a1a76ae626819e0d09a7e33fff83eade9
Seth <sethnejame@gmail.com>
2020-02-01 14:19:05 +0100

    CampMapping now pulls in venue name from Location entity as 'venue'

--- starting/TheCodeCamp/Controllers/CampsController.cs
+++ starting/TheCodeCamp/Controllers/CampsController.cs
@@ -56,5 +56,6 @@ namespace TheCodeCamp.Controllers
                 return InternalServerError(ex);
             }
         }
+
     }
 }
--- starting/TheCodeCamp/Models/CampModel.cs
+++ starting/TheCodeCamp/Models/CampModel.cs
@@ -11,6 +11,16 @@ namespace TheCodeCamp.Models
         public string Name { get; set; }
         public string Moniker { get; set; }
         public DateTime EventDate { get; set; } = DateTime.MinValue;
-        public int Length { get; set; } = 1;     
+        public int Length { get; set; } = 1;
+
+        // Include Location Info
+        public string LocationVenueName { get; set; }
+        public string LocationAddress1 { get; set; }
+        public string LocationAddress2 { get; set; }
+        public string LocationAddress3 { get; set; }
+        public string LocationCityTown { get; set; }
+        public string LocationStateProvince { get; set; }
+        public string LocationPostalCode { get; set; }
+        public string LocationCountry { get; set; }
     }
 }
